be2f8398aec392be28d114d08c87132a60383512
Yeasin Arafat <yachenarafath@gmail.com>
2024-07-14 22:23:23 +0600

    Added Subscribe to Newslater Box

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,14 @@
+import SubscribeToNewsLater from "@/components/Shared/SubscribeToNewsLater";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Button>Hello</Button>
-    </div>
+    <>
+      <div className="min-h-screen">
+        <Button>Hello</Button>
+      </div>
+      <SubscribeToNewsLater />
+    </>
   );
 }
