1d81e2f1b4f8247c051d9c7b61508adf6192f9a1
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-07-15 18:34:31 +0600

    hero sections slider added

--- src/components/Shared/NavBar/NavBar.tsx
+++ src/components/Shared/NavBar/NavBar.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-
 import TopBar from "../TopBar";
 import MiddleBar from "./MiddleBar";
 import BottomBar from "./BottomBar";
 import MobileMiddleNav from "./MobileNav";
 
 const NavBar = () => {
-  return (
-    <>
-      <TopBar />
-      <MiddleBar />
-      <MobileMiddleNav />
-      <BottomBar />
-    </>
-  );
+    return (
+        <>
+            <TopBar />
+            <MiddleBar />
+            <MobileMiddleNav />
+            <BottomBar />
+        </>
+    );
 };
 
 export default NavBar;
