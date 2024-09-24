a88418e0ee4fbb96dfc1c57d636ebb908bf7efed
Yeasin Arafat <yachenarafath@gmail.com>
2024-09-25 00:12:31 +0600

    added clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,23 @@
-export { auth as middleware } from "@/auth";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// export default auth((req) => {
-//   if (!req.auth && req.nextUrl.pathname !== "/login") {
-//     const newUrl = new URL("/login", req.nextUrl.origin);
-//     return Response.redirect(newUrl);
-//   }
-// });
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/about-us",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
